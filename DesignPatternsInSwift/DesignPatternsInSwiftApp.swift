7c7a19897b781d503b38fef77b1865ab9600431f
Damian <hello@damianruta.pl>
2022-11-08 19:04:40 +0100

    Initial Commit

---
+++ DesignPatternsInSwift/DesignPatternsInSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  DesignPatternsInSwiftApp.swift
+//  DesignPatternsInSwift
+//
+//  Created by Damian Ruta on 08/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct DesignPatternsInSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
